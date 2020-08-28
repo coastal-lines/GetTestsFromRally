e85c369e84ab690b3768facf0d4d1ce2667bc5b3
paul <piankin250@gmail.com>
2020-08-28 15:20:41 +0300

    Get one testcase

--- Rally/Rally/TestCaseStructure/TestCase.cs
+++ Rally/Rally/TestCaseStructure/TestCase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rally.RestApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,43 @@ namespace Rally.TestCaseStructure
         public string ObjectID { get; set; }
         public string TestCaseNumber { get; set; }
         public string TestCaseName { get; set; }
+        public List<Step> Steps { get; private set; }
 
-        public TestCase(dynamic result)
+        private dynamic rallyResult;
+        private RallyRestApi api;
+
+        public TestCase(dynamic result, RallyRestApi api)
         {
-            ObjectID = result["ObjectID"];
-            TestCaseNumber = result["FormattedID"];
-            TestCaseName = result["Name"];
+            this.rallyResult = result;
+            this.api = api;
+
+            ObjectID = rallyResult["ObjectID"].ToString();
+            TestCaseNumber = rallyResult["FormattedID"].ToString();
+            TestCaseName = rallyResult["Name"].ToString();
+
+            InitiliazeStepsFromTestCase();
+        }
+
+        public void InitiliazeStepsFromTestCase()
+        {
+            var resultSteps = rallyResult["Steps"];
+            var stepsRef = resultSteps["_ref"];
+            var stepsFromRally = api.GetByReference(stepsRef);
+
+            if (stepsFromRally.TotalResultCount > 0)
+            {
+                Steps = new List<Step>();
+
+                foreach (var step in stepsFromRally.Results)
+                {
+                    Steps.Add(new Step()
+                    {
+                        StepNumber = step["StepIndex"],
+                        ExpectedText = step["ExpectedResult"],
+                        InputText = step["Input"]
+                    });
+                }
+            }
         }
     }
 }
