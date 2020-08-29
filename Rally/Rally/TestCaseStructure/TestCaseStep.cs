ca1f6e3c87bf4e30cffae688e4a65998b262f424
paul <piankin250@gmail.com>
2020-08-29 16:12:53 +0300

    Get one testcase

---
+++ Rally/Rally/TestCaseStructure/TestCaseStep.cs
@@ -0,0 +1,9 @@
+﻿namespace Rally.TestCaseStructure
+{
+    public class TestCaseStep
+    {
+        public int StepNumber { get; set; }
+        public string InputText { get; set; }
+        public string ExpectedText { get; set; }
+    }
+}
