5b03a6b62c316dab1de4e5c8681f13459fae263f
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2020-09-11 23:05:03 +0300

    Delete TestCaseStep.cs

--- Rally/Rally/TestCaseStructure/TestCaseStep.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rally.TestCaseStructure
-{
-    public class TestCaseStep
-    {
-        public int StepNumber { get; set; }
-        public string InputText { get; set; }
-        public string ExpectedText { get; set; }
-    }
-}
